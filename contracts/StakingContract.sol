a197727e356c6025620db3db025a3b39b4faa78e
Banwo Olorunsogo <sogobanwo@gmail.com>
2024-02-17 15:31:46 +0100

    Feat: Added all function of the staking contract

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -3,33 +3,109 @@ pragma solidity ^0.8.0;
 import "./IStakeToken.sol";
 
 contract StakingContract {
-    uint stakinDuration;
-    uint rewardRate;
-    uint maxStake;
-    uint minStake;
+    // STATE VARIABLES
+    address stakeToken;
+    address owner;
+    uint immutable stakingDuration;
+    uint immutable inverseRewardRate;
+    uint immutable maxStake;
+    uint immutable minStake;
+    uint immutable minDeposit;
+    uint immutable maxDeposit;
+    uint availableRewardTokens;
 
+    // MAPPINGS
     mapping (address => uint) amountStaked;
     mapping (address => uint) walletBalance;
     mapping (address => uint) reward;
+    mapping (address => uint) rewardTimeStamp;
 
-    constructor(address _stakeToken, uint _stakinDuration, uint _rewardRate, uint _maxStake, uint _minStake) {
-        
+    constructor(address _stakeToken, uint _stakingDuration, uint _inverseRewardRate, uint _minDeposit, uint _maxDeposit, uint _maxStake, uint _minStake) {
+        stakeToken = _stakeToken;
+        owner = msg.sender;
+        stakingDuration = _stakingDuration;
+        inverseRewardRate = _inverseRewardRate;
+        minStake = _minStake;
+        maxStake = _maxStake;
+        minDeposit = _minDeposit;
+        maxDeposit = _maxDeposit;
+    }
+
+    // EVENTS
+    event SuccesfulDeposit(address sender, uint depositAmount);
+    event SuccesfulStake(address sender, uint stakeAmount);
+    event SuccesfulUnstaking(address sender, uint unstakedAmount);
+    event RewardClaimed(address sender, uint claimedAmount);
+    event WithdrawSuccessful(address sender, uint _amountWithdrawn);
+
+    // FUNCTIONS
+    function depositToWallet(uint _amount) external{
+        require(msg.sender != address(0), "Address zero detected");
+        require(_amount > 0, "cannot deposit 0 token");
+        require(_amount >= minDeposit, "Not up to the min deposit");
+        require(_amount <= maxDeposit, "more than the max deposit");
+        require(IStakeToken(stakeToken).balanceOf(msg.sender) > _amount, "Not enough token");
+        require(IStakeToken(stakeToken).transferFrom(msg.sender, address(this), _amount), "Failed Deposit");
+        walletBalance[msg.sender] = walletBalance[msg.sender] + _amount;
+        emit SuccesfulDeposit(msg.sender, _amount);
+    }
+
+    function setAvailableReward() external  {
+        availableRewardTokens = IStakeToken(stakeToken).balanceOf(address(this));
     }
 
     function stake(uint _amount) external{
-        
+        require(msg.sender != address(0), "Address zero detected");
+        require(_amount > 0, "cannot stake 0 token");
+        require(_amount >= minStake, "Not up to min Stake");
+        require(_amount <= maxStake, "more than max Stake");
+        require(walletBalance[msg.sender] > _amount, "Not enough balance in wallet");
+        require(availableRewardTokens >= 1000, "Rewards exhausted");
+        walletBalance[msg.sender] = walletBalance[msg.sender] - _amount;
+        amountStaked[msg.sender] = amountStaked[msg.sender] + _amount;
+        setReward();
+        rewardTimeStamp[msg.sender] = block.timestamp;
+        emit SuccesfulStake(msg.sender, _amount);
     }
 
-    function Unstake(uint _unstake) external{
-        
+    function Unstake() external{
+        require(msg.sender != address(0), "Address zero detected");
+        require(amountStaked[msg.sender] != 0, "You don't have any stake");
+        amountStaked[msg.sender] = 0;
+        reward[msg.sender] = 0;
+        rewardTimeStamp[msg.sender] = 0;
+        walletBalance[msg.sender] = walletBalance[msg.sender] + amountStaked[msg.sender];
+        emit SuccesfulUnstaking(msg.sender, amountStaked[msg.sender]);
+    }
+
+    function setReward() private {
+      reward[msg.sender] = amountStaked[msg.sender] * stakingDuration / inverseRewardRate;
+    }
+
+    function calculateReward(uint _amount) external view returns(uint){
+        require(_amount != 0, "can't calculate reward for the value of 0");
+        return _amount  * stakingDuration / inverseRewardRate;
     }
     
-    function claimReward(address _staker) external{ 
-        
+    function claimRewardToWallet() external{ 
+        require(msg.sender != address(0), "Address zero detected");
+        require(reward[msg.sender] != 0, "You don't have any reward");
+        require(block.timestamp >= rewardTimeStamp[msg.sender]  + stakingDuration);       
+        reward[msg.sender] = 0;
+        amountStaked[msg.sender] = 0;
+        walletBalance[msg.sender] = walletBalance[msg.sender] + reward[msg.sender] + amountStaked[msg.sender]; 
+        emit RewardClaimed(msg.sender, reward[msg.sender]);
     }
 
-    function withdraw(uint _amount, address _staker) external{
-        
+    function withdrawFromWallet(uint _amount) external{
+        require(msg.sender != address(0), "Address zero detected");
+        require(walletBalance[msg.sender] > _amount, "You don't have enough balance in your wallet");
+
+        walletBalance[msg.sender] = walletBalance[msg.sender] - _amount;
+
+        require(IStakeToken(stakeToken).transfer(msg.sender, _amount), "failed to withdraw");
+
+        emit WithdrawSuccessful(msg.sender, _amount);
     }
 
     function getStake(address __staker) external view  returns (uint) {
@@ -40,6 +116,14 @@ contract StakingContract {
         return reward[__staker];
     }
 
+    function getWalletBalance(address __staker) external view returns (uint) {
+        return walletBalance[__staker];
+    }
+
+    function getTotalWalletBalance() external view returns (uint) {
+        return walletBalance[address(this)];
+    }
+
     function getTotalStake() external view returns (uint)  {
         return amountStaked[address(this)];
     }
@@ -47,7 +131,4 @@ contract StakingContract {
      function getTotalReward() external view returns (uint)  {
         return reward[address(this)];
     }
-
-
-
 }
